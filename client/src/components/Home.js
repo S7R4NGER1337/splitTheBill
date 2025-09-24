a59f465d8b9360ed3c4adce8b3e27de0d2e23083
S7R4NGER3301 <nikolakoew@gmail.com>
2025-09-24 17:43:34 +0300

    formatting document

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,16 +1,23 @@
-import styles from './home.module.css'
+import styles from "./home.module.css";
 
 export default function Home() {
-    return(
-        <div className={styles.homeContainer}>
-            <section className={styles.homeData}>
-                <h1 className={styles.homeDataTitle}>Welcome to SplitTheBill</h1>
-                <p className={styles.homeDataSubtitle}>Easily split bills with friends and family. Get started by scanning your receipt.</p>
-            </section>
-            <button className={styles.homeButton}>
-                <img className={styles.homeButtonIcon} src='/camera-regular-full.svg' alt='cameraIcon' />
-                Scan Receipt
-            </button>
-        </div>
-    )
+  return (
+    <div className={styles.homeContainer}>
+      <section className={styles.homeData}>
+        <h1 className={styles.homeDataTitle}>Welcome to SplitTheBill</h1>
+        <p className={styles.homeDataSubtitle}>
+          Easily split bills with friends and family. Get started by scanning
+          your receipt.
+        </p>
+      </section>
+      <button className={styles.homeButton}>
+        <img
+          className={styles.homeButtonIcon}
+          src="/camera-regular-full.svg"
+          alt="cameraIcon"
+        />
+        Scan Receipt
+      </button>
+    </div>
+  );
 }
