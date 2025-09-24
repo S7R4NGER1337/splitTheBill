b1b37a7ca66279dba8a9c1fdbc76fa4fef8238ff
S7R4NGER3301 <nikolakoew@gmail.com>
2025-09-24 18:08:11 +0300

    creating scan receipt component

---
+++ client/src/components/scanReceipt/ScanReceipt.js
@@ -0,0 +1,9 @@
+import styles from './scanReceipt.module.css'
+
+export default function ScanReceipt() {
+    return(
+        <div className={styles.scanReceiptContainer}>
+            
+        </div>
+    )
+}
