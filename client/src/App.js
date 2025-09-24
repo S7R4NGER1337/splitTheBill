7b2e53357c7d1070650cb4d7a4ef68c8b4007daf
S7R4NGER3301 <nikolakoew@gmail.com>
2025-09-24 19:51:41 +0300

    setting home and scan routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import "./App.css";
 import Home from "./components/Home";
 import Navigation from "./components/Navigation";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import ScanReceipt from "./components/scanReceipt/ScanReceipt";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/scanReceipt" element={<ScanReceipt />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
