06bdc2ee4358dc79b1ddfb7444143e8bd1e083a2
S7R4NGER3301 <nikolakoew@gmail.com>
2025-09-24 19:20:48 +0300

    deleting button component and adding inputs

--- client/src/components/scanReceipt/Button.js
+++
@@ -1,9 +0,0 @@
-import styles from './button.module.css'
-
-export default function Button({type, children}) {
-    return(
-        <button className={type === 'primary' ? styles.primary : styles.secondary}>
-            {children}
-        </button>
-    )
-}
--- client/src/components/scanReceipt/ScanReceipt.js
+++ client/src/components/scanReceipt/ScanReceipt.js
@@ -1,9 +1,43 @@
-import styles from './scanReceipt.module.css'
+import styles from "./scanReceipt.module.css";
 
 export default function ScanReceipt() {
-    return(
-        <div className={styles.scanReceiptContainer}>
-            
-        </div>
-    )
+  return (
+    <div className={styles.scanReceiptContainer}>
+      <section className={styles.scanReceiptData}>
+        <img
+          className={styles.scanReceiptIcon}
+          src="/receipt-regular-full.svg"
+          alt="receiptIcon"
+        />
+        <h1 className={styles.scanReceiptTitle}>Scan your receipt</h1>
+        <p className={styles.scanReceiptSubtitle}>
+          Position your receipt within the frame. We'll handle the rest.
+        </p>
+      </section>
+      <div className={styles.scanReceiptButtonsContaienr}>
+         <label className={`${styles.photoLabel} ${styles.takePhotoWrap}`}>
+          <input
+            className={styles.takePhotoInput}
+            type="file"
+            placeholder="Take Photo"
+            accept="image/*"
+            capture="environment"
+          />
+          <img className={styles.takePhotoIcon} src="/camera-regular-full.svg" alt="cameraIcon" />
+          Take Photo
+        </label>
+        <label className={`${styles.photoLabel} ${styles.selectPhotoWrap}`}>
+          <input
+            className={styles.takePhotoInput}
+            type="file"
+            placeholder="Take Photo"
+            accept="image/*"
+            capture="environment"
+          />
+          <img className={styles.takePhotoIcon} src="/camera-regular-full.svg" alt="cameraIcon" />
+          Select from Library
+        </label>
+      </div>
+    </div>
+  );
 }
