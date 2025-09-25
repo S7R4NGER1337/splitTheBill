fd8dd3d12244db0d51363c86f4b635a845c2ae3a
S7R4NGER3301 <nikolakoew@gmail.com>
2025-09-25 20:08:24 +0300

    rendering all items from the receipt

--- client/src/components/receiptPage/ReceiptPage.js
+++ client/src/components/receiptPage/ReceiptPage.js
@@ -2,6 +2,7 @@ import styles from './receiptPage.module.css'
 import ReceiptClients from './ReceiptClients'
 import { receipt } from '../../api/staticData'
 import { useState } from 'react'
+import ReceiptItem from './ReceiptItem'
 
 export default function ReceiptPage() {
     const [receiptData, setReceiptData] = useState(receipt)
@@ -9,6 +10,9 @@ export default function ReceiptPage() {
     return (
         <div className={styles.receiptPageContainer}>
             <ReceiptClients receiptData={receiptData} setReceiptData={setReceiptData}/>
+            <div className={styles.receiptItemsContainer}>
+                {receiptData.orderItems.map(item => <ReceiptItem itemData={item}/>)}
+            </div>
         </div>
     )
 }
