0471f6bd3bdacccf88735128f2f85c9026dd1b83
S7R4NGER3301 <nikolakoew@gmail.com>
2025-09-25 19:48:56 +0300

    rendering each letter and button to add clients

--- client/src/components/receiptPage/ReceiptClients.js
+++ client/src/components/receiptPage/ReceiptClients.js
@@ -1,12 +1,13 @@
-import styles from './receiptClient.module.css'
+import styles from './receiptClients.module.css'
 
 export default function ReceiptClients({receiptData}) {
     const letters = receiptData.clients.map(client => client[0])
 
     return <div className={styles.receiptClientContainer}>
-        <h1>Receipt Clients</h1>
+        <h1 className={styles.receiptClientTitle}>Receipt Clients</h1>
         <div className={styles.receptClinetPhotos}>
-            {letters.map(letter => <p>{letter}</p>)}
+            {letters.map((letter, index) => <p className={styles.receiptClientPhoto} key={index}>{letter}</p>)}
+            <button className={styles.receiptClientButton}>+</button>
         </div>
     </div>
 }
