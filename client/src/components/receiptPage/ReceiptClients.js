39248f8b80ef2383101571e5814c736a26299196
S7R4NGER3301 <nikolakoew@gmail.com>
2025-09-25 19:56:16 +0300

    rendering new client photos

--- client/src/components/receiptPage/ReceiptClients.js
+++ client/src/components/receiptPage/ReceiptClients.js
@@ -1,13 +1,18 @@
-import styles from './receiptClients.module.css'
+import ReceiptClientPhoto from "./ReceiptClientPhoto";
+import styles from "./receiptClients.module.css";
 
-export default function ReceiptClients({receiptData}) {
-    const letters = receiptData.clients.map(client => client[0])
+export default function ReceiptClients({ receiptData }) {
+  const letters = receiptData.clients.map((client) => client[0]);
 
-    return <div className={styles.receiptClientContainer}>
-        <h1 className={styles.receiptClientTitle}>Receipt Clients</h1>
-        <div className={styles.receptClinetPhotos}>
-            {letters.map((letter, index) => <p className={styles.receiptClientPhoto} key={index}>{letter}</p>)}
-            <button className={styles.receiptClientButton}>+</button>
-        </div>
+  return (
+    <div className={styles.receiptClientContainer}>
+      <h1 className={styles.receiptClientTitle}>Receipt Clients</h1>
+      <div className={styles.receptClinetPhotos}>
+        {letters.map((letter, index) => (
+          <ReceiptClientPhoto key={index} letter={letter} />
+        ))}
+        <button className={styles.receiptClientButton}>+</button>
+      </div>
     </div>
+  );
 }
