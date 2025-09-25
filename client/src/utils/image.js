48ec404f4b0354f8bee78953a202f04cfaaba3bc
S7R4NGER3301 <nikolakoew@gmail.com>
2025-09-25 14:38:47 +0300

    moving send image handler in utils file

---
+++ client/src/utils/image.js
@@ -0,0 +1,11 @@
+import { sendImage } from '../api/image'
+
+export const handleChange = async (e) => {
+  const file = e.target.files[0];
+  if (!file) return;
+
+  const formData = new FormData();
+  formData.append("image", file);
+
+ const receipt = await sendImage(formData)
+};
