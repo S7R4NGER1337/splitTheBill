89e1857865c7be936aa7c11488dadd5541b51400
S7R4NGER3301 <nikolakoew@gmail.com>
2025-09-25 20:28:50 +0300

    creating receipt item component

--- client/src/components/receiptPage/ReceiptItem.js
+++ client/src/components/receiptPage/ReceiptItem.js
@@ -1,10 +1,20 @@
-import styles from './receiptItem.module.css'
+import styles from "./receiptItem.module.css";
 
-export default function ReceiptItem({itemData}) {
-    return(
-        <div className={styles.receiptItemContainer}>
-            <h1>{itemData.name}</h1>
+export default function ReceiptItem({ itemData }) {
 
+  return (
+    <div className={styles.receiptItemContainer}>
+      <div className={styles.receiptItemData}>
+        <section className={styles.receiptItemInfo}>
+          <h1 className={styles.receiptItemName}>{itemData.name}</h1>
+          <p className={styles.receiptItemPrice}>${itemData.price} x{itemData.qty}</p>
+        </section>
+        <div className={styles.receiptItemActions}>
+          <img className={styles.receiptAction} src="/pencil-solid-full.svg" alt="edit" />
+          <img className={styles.receiptAction} src="/trash-solid-full.svg" alt="edit" />
         </div>
-    )
+      </div>
+
+    </div>
+  );
 }
