c0e59ffd8c8dae0b354ff160ac3d6c1a6efe621e
S7R4NGER3301 <nikolakoew@gmail.com>
2025-09-25 19:32:43 +0300

    getting static data from a file and passing it to a ReceiptClients component

--- client/src/components/receiptPage/ReceiptPage.js
+++ client/src/components/receiptPage/ReceiptPage.js
@@ -1,10 +1,14 @@
 import styles from './receipt.module.css'
 import ReceiptClients from './ReceiptClients'
+import { receipt } from '../../api/staticData'
+import { useState } from 'react'
 
 export default function ReceiptPage() {
+    const [receiptData, setReceiptData] = useState(receipt)
+    
     return (
         <div className={styles.receiptPageContainer}>
-            <ReceiptClients />
+            <ReceiptClients receiptData={receiptData} setReceiptData={setReceiptData}/>
         </div>
     )
 }
