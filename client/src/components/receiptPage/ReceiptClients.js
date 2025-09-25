5c92e53c5163c9b6b2f0ed2ed80ab3c66955db41
S7R4NGER3301 <nikolakoew@gmail.com>
2025-09-25 19:33:07 +0300

    Rendering first letter for every name in ReceiptClients

--- client/src/components/receiptPage/ReceiptClients.js
+++ client/src/components/receiptPage/ReceiptClients.js
@@ -1,3 +1,12 @@
-export default function ReceiptClients(params) {
-    return <h1>Clients</h1>
+import styles from './receiptClient.module.css'
+
+export default function ReceiptClients({receiptData}) {
+    const letters = receiptData.clients.map(client => client[0])
+
+    return <div className={styles.receiptClientContainer}>
+        <h1>Receipt Clients</h1>
+        <div className={styles.receptClinetPhotos}>
+            {letters.map(letter => <p>{letter}</p>)}
+        </div>
+    </div>
 }
