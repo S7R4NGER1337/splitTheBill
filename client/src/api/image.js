3af770efc6795bf22b5df5f52e003ff17e05e4f8
S7R4NGER3301 <nikolakoew@gmail.com>
2025-09-26 18:26:28 +0300

    creating set name function on frontend

--- client/src/api/image.js
+++ client/src/api/image.js
@@ -1,4 +1,4 @@
-const URL = "http://localhost:3030"
+const URL = "http://localhost:3030";
 
 export async function sendImage(formData) {
   try {
@@ -8,14 +8,27 @@ export async function sendImage(formData) {
     });
 
     const data = await response.json();
-    return data
+    return data;
   } catch (err) {
     console.error("Upload error");
   }
 }
 
 export async function GetReceiptById(id, setReceiptData) {
-  const response = await fetch(`${URL}/receipt/${id}`)
-  const data = await response.json()
-  setReceiptData(data)
+  const response = await fetch(`${URL}/receipt/${id}`);
+  const data = await response.json();
+  setReceiptData(data);
+}
+
+export async function setNewName(id, name) {
+  const response = await fetch(`${URL}/receipt/setUser`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ id: id, name: name }),
+  });
+
+  const data = await response.json();
+  return data;
 }
