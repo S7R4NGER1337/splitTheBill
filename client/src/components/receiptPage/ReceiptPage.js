f8258180797cf1bc8d012c6927901029c02e4fd0
S7R4NGER3301 <nikolakoew@gmail.com>
2025-09-26 18:17:54 +0300

    fixing problem with data rendering

--- client/src/components/receiptPage/ReceiptPage.js
+++ client/src/components/receiptPage/ReceiptPage.js
@@ -1,31 +1,37 @@
-import styles from './receiptPage.module.css'
-import ReceiptClients from './ReceiptClients'
-import { useEffect, useState } from 'react'
-import ReceiptItem from './ReceiptItem'
-import { useLocation } from 'react-router-dom'
-import { GetReceiptById } from '../../api/image'
-import Loader from '../Loader'
+import styles from "./receiptPage.module.css";
+import ReceiptClients from "./ReceiptClients";
+import { useEffect, useState } from "react";
+import ReceiptItem from "./ReceiptItem";
+import { useLocation } from "react-router-dom";
+import { GetReceiptById } from "../../api/image";
+import Loader from "../Loader";
 
 export default function ReceiptPage() {
-    const [receiptData, setReceiptData] = useState(null)
-    const location = useLocation()
+  const [receiptData, setReceiptData] = useState(null);
+  const location = useLocation();
 
-    const receiptId = location.pathname.split('/')[2]
-    useEffect(() => {
-        if(location.state !== undefined) {
-            setReceiptData(location.state.receiptData)
-        } else {
-            GetReceiptById(receiptId, setReceiptData)
-        }
-    },[])
-    
-    return (
-        receiptData !== null ?
-        <div className={styles.receiptPageContainer}>
-            <ReceiptClients receiptData={receiptData} setReceiptData={setReceiptData}/>
-            <div className={styles.receiptItemsContainer}>
-                {receiptData.orderItems.map((item, index) => <ReceiptItem itemData={item} receiptData={receiptData} key={index}/>)}
-            </div>
-        </div>: <></>
-    )
+  const receiptId = location.pathname.split("/")[2];
+  useEffect(() => {
+    if (location.state !== null) {
+      setReceiptData(location.state.receiptData);
+    } else {
+      GetReceiptById(receiptId, setReceiptData);
+    }
+  }, []);
+
+  return receiptData !== null ? (
+    <div className={styles.receiptPageContainer}>
+      <ReceiptClients
+        receiptData={receiptData}
+        setReceiptData={setReceiptData}
+      />
+      <div className={styles.receiptItemsContainer}>
+        {receiptData.orderItems.map((item, index) => (
+          <ReceiptItem itemData={item} receiptData={receiptData} key={index} />
+        ))}
+      </div>
+    </div>
+  ) : (
+    <></>
+  );
 }
