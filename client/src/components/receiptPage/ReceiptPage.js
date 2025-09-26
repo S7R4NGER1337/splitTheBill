ded550a38916c36fc07dc348292645a3b4c04341
S7R4NGER3301 <nikolakoew@gmail.com>
2025-09-26 17:28:11 +0300

    rendering receipt data if user is redirected and fetching if he just opened the page

--- client/src/components/receiptPage/ReceiptPage.js
+++ client/src/components/receiptPage/ReceiptPage.js
@@ -1,18 +1,31 @@
 import styles from './receiptPage.module.css'
 import ReceiptClients from './ReceiptClients'
-import { receipt } from '../../api/staticData'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import ReceiptItem from './ReceiptItem'
+import { useLocation } from 'react-router-dom'
+import { GetReceiptById } from '../../api/image'
+import Loader from '../Loader'
 
 export default function ReceiptPage() {
-    const [receiptData, setReceiptData] = useState(receipt)
+    const [receiptData, setReceiptData] = useState(null)
+    const location = useLocation()
+
+    const receiptId = location.pathname.split('/')[2]
+    useEffect(() => {
+        if(location.state !== undefined) {
+            setReceiptData(location.state.receiptData)
+        } else {
+            GetReceiptById(receiptId, setReceiptData)
+        }
+    },[])
     
     return (
+        receiptData !== null ?
         <div className={styles.receiptPageContainer}>
             <ReceiptClients receiptData={receiptData} setReceiptData={setReceiptData}/>
             <div className={styles.receiptItemsContainer}>
                 {receiptData.orderItems.map((item, index) => <ReceiptItem itemData={item} receiptData={receiptData} key={index}/>)}
             </div>
-        </div>
+        </div>: <></>
     )
 }
