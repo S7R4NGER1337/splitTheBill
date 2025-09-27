fcaadb5fa2623ebc5d6010adad20a82377e8bfff
S7R4NGER3301 <nikolakoew@gmail.com>
2025-09-27 16:22:46 +0300

    rendering every client name in every product

--- client/src/components/receiptPage/ReceiptItem.js
+++ client/src/components/receiptPage/ReceiptItem.js
@@ -1,20 +1,34 @@
 import styles from "./receiptItem.module.css";
+import ReceiptClientPhoto from "./ReceiptClientPhoto";
 
-export default function ReceiptItem({ itemData }) {
-
+export default function ReceiptItem({ itemData, receiptData }) {
   return (
     <div className={styles.receiptItemContainer}>
       <div className={styles.receiptItemData}>
         <section className={styles.receiptItemInfo}>
           <h1 className={styles.receiptItemName}>{itemData.name}</h1>
-          <p className={styles.receiptItemPrice}>${itemData.price} x{itemData.qty}</p>
+          <p className={styles.receiptItemPrice}>
+            ${itemData.price} x{itemData.qty}
+          </p>
         </section>
         <div className={styles.receiptItemActions}>
-          <img className={styles.receiptAction} src="/pencil-solid-full.svg" alt="edit" />
-          <img className={styles.receiptAction} src="/trash-solid-full.svg" alt="edit" />
+          <img
+            className={styles.receiptAction}
+            src="/pencil-solid-full.svg"
+            alt="edit"
+          />
+          <img
+            className={styles.receiptAction}
+            src="/trash-solid-full.svg"
+            alt="edit"
+          />
         </div>
       </div>
-
+      <div className={styles.clientsSelectContainer}>
+        {receiptData.clients.map((name, index) => (
+          <ReceiptClientPhoto key={index} name={name} type={"secondary"} selectedClients={itemData.orderedBy}/>
+        ))}
+      </div>
     </div>
   );
 }
