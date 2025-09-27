3cc6eec47625b0807e78ab3175874e92b08334e1
S7R4NGER3301 <nikolakoew@gmail.com>
2025-09-27 16:22:23 +0300

    setting type to client name to conditional render styles

--- client/src/components/receiptPage/ReceiptClientPhoto.js
+++ client/src/components/receiptPage/ReceiptClientPhoto.js
@@ -1,5 +1,26 @@
-import styles from './receiptClientPhoto.module.css'
+import styles from "./receiptClientPhoto.module.css";
+import { useState, useEffect } from "react";
 
-export default function ReceiptClientPhoto({letter}){
-    return <p className={styles.receiptClientPhoto}>{letter}</p>
+export default function ReceiptClientPhoto({ name, type, selectedClients }) {
+  const [nameType, setNameType] = useState(type);
+
+  useEffect(() => {
+    if (Array.isArray(selectedClients)) {
+      if (selectedClients.includes(name)) {
+        setNameType("main");
+      } else {
+        setNameType("secondary");
+      }
+    }
+  }, [selectedClients, name]);
+
+  return (
+    <p
+      className={`${styles.receiptClientPhoto} ${
+        nameType === "main" ? styles.main : styles.secondary
+      }`}
+    >
+      {name}
+    </p>
+  );
 }
