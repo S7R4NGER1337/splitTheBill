d8d853129ac88b762a3ccdb55614b7126d3327f2
S7R4NGER3301 <nikolakoew@gmail.com>
2025-09-28 15:27:00 +0300

    adding id to every item

--- client/src/components/receiptPage/summary/SummaryOrderedItems.js
+++ client/src/components/receiptPage/summary/SummaryOrderedItems.js
@@ -4,7 +4,7 @@ export default function SummaryOrderedItems({ orderedItems }) {
   return (
     <div className={styles.orderedItems}>
       {orderedItems.map((item) => (
-        <p className={styles.orderdItem}>{item.name} (${item.price})</p>
+        <p className={styles.orderdItem} key={item._id}>{item.name} (${item.price})</p>
       ))}
     </div>
   );
