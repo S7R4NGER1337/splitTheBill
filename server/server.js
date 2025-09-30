11c9bac37365e61d513ce1000adaff5debc73732
S7R4NGER3301 <nikolakoew@gmail.com>
2025-09-30 18:56:34 +0300

    delete cors options

--- server/server.js
+++ server/server.js
@@ -14,10 +14,6 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-let corsOptions = {
-  origin: [process.env.FRONTEND_URL],
-};
-
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
