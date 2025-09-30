478ad09c388df310894f235877d6658d420d2a3e
S7R4NGER3301 <nikolakoew@gmail.com>
2025-09-30 17:27:50 +0300

    passing correct data

--- client/src/api/image.js
+++ client/src/api/image.js
@@ -63,15 +63,19 @@ export async function deleteReceiptItem(itemId, receiptId) {
   return deletedProduct;
 }
 
-export async function editReceiptItem(itemData) {
+export async function editReceiptItem(itemData, receiptDataId) {
   const response = await fetch(`${URL}/receiptItem/edit`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ itemId: itemData._id, itemData: itemData }),
+    body: JSON.stringify({
+      itemId: itemData._id,
+      itemData: itemData,
+      receiptId: receiptDataId,
+    }),
   });
-  const editedItem = await response.json()
+  const editedItem = await response.json();
 
-  return editedItem
+  return editedItem;
 }
